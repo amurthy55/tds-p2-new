4c6aa642de0ec68673ee40dc3df3678b6d6efbad
amurthy55 <25ds2000003@ds.study.iitm.ac.in>
2025-11-25 11:17:12 +0530

    Remove .env leaked secret

--- quizsolver/config.py
+++ quizsolver/config.py
@@ -1,10 +1,11 @@
 import os
 
 STUDENT_EMAIL = "25ds2000003@ds.study.iitm.ac.in"
-STUDENT_SECRET = "0ct0b3r"
+
 
 # Choose which LLM to use
 USE_AIPIPE = True
 
 AIPIPE_API_KEY = os.getenv("AIPIPE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+STUDENT_SECRET = os.getenv("STUDENT_SECRET")
